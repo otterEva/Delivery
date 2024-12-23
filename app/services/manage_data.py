4f53485b342ec93d2f2f62d8fa6d5ab980fb276a
Ivan <iskiba60@gmail.com>
2024-12-23 23:41:56 +0300

    Добавил логирование

--- app/services/manage_data.py
+++ app/services/manage_data.py
@@ -1,21 +1,19 @@
 class UpdateDataService:
+    def update_dataholder(self, dataholder, ready_responce):
+        for responce in ready_responce["routes"]:
+            real_sources = responce[1]["real_sources"]
+            real_tagrets = responce[1]["real_targets"]
 
-	def update_dataholder(self, dataholder, ready_responce):
+            j = 0
 
-		for responce in ready_responce["routes"]:
+            for route in responce[0]["routes"]:
+                distance_ = route["distance"]
+                duration_ = route["duration"]
+                dataholder[real_sources]["object"].distance[real_tagrets[j]] = distance_
+                dataholder[real_sources]["object"].duration[real_tagrets[j]] = duration_
+                j += 1
 
-			real_sources = responce[1]["real_sources"]
-			real_tagrets = responce[1]["real_targets"]
+        return dataholder
 
-			j=0
-
-			for route in responce[0]['routes']:
-				distance_ =  route["distance"]
-				duration_ =  route["duration"]
-				dataholder[real_sources]['object'].distance[real_tagrets[j]] = distance_
-				dataholder[real_sources]['object'].duration[real_tagrets[j]] = duration_
-				j += 1
-
-		return dataholder
 
 DataManageService = UpdateDataService()
---
+++ dockerfile
@@ -0,0 +1,17 @@
+FROM python:3.12-slim
+
+WORKDIR /app
+
+
+RUN apt-get update && apt-get install -y --no-install-recommends curl && \
+    curl -sSL https://install.python-poetry.org | python3 - && \
+    ln -s /root/.local/bin/poetry /usr/local/bin/poetry && \
+    apt-get remove -y curl && apt-get autoremove -y && \
+    rm -rf /var/lib/apt/lists/*
+
+COPY pyproject.toml poetry.lock ./
+
+RUN poetry config virtualenvs.create false && poetry install --no-dev
+COPY . .
+
+CMD ["poetry", "run", "python", "cli.py", "api"]
